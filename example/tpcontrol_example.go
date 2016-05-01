811a94c02bff5a6001e055c51e52815723cf6e24
Hekmon <edouardhur@gmail.com>
2016-05-02 00:40:46 +0200

    update/condensate example

--- example/tpcontrol_example.go
+++ example/tpcontrol_example.go
@@ -7,11 +7,11 @@ import (
 )
 
 func main() {
-	// Let's go for a throughput of 5 requests by second, 3 differents priorities and and 2 tokens pool size
+	// Let's go for a throughput of 5 requests by second, 3 differents priorities and and 5 tokens pool size
 	flowNbRequests := 5
 	flowNbSeconds  := 1
 	nbQueues       := 3
-	tokenPoolSize  := 2
+	tokenPoolSize  := 5
 	scheduler, err := tpcontrol.New(flowNbRequests, flowNbSeconds, nbQueues, tokenPoolSize)
 	if err != nil {
 		panic(err)
@@ -26,25 +26,21 @@ func main() {
 	fmt.Println("Time's up !")
 
 	// Spawn priority workers by batches
-	nbBatches := 5
 	notifEnded := make(chan bool)
 	launchStarted := time.Now()
+	nbBatches := 4
 	for currentBatch := 0 ; currentBatch < nbBatches ; currentBatch++ {
 		// One per queue (queue 0 == highest priority)
 		for currentQueue := 0 ; currentQueue < nbQueues ; currentQueue++ {
 			// Be carefull with goroutines and scope
 			localBatch := currentBatch
 			localQueue := currentQueue
-
 			// Launch worker
 			go func() {
-				// So our worker wants to work... great ! Let's ask the scheduler.
 				scheduler.CanIGO(localQueue) // This call will block until the scheduler let us work
-
-				fmt.Printf("I am a worker with a priority of %d coming from the batch %d and this experiment started %v ago.\n", localQueue, localBatch, time.Since(launchStarted))
-
-				// Tell the main goroutine this worker is done
-				notifEnded <- true
+				fmt.Printf("I am a worker with a priority of %d coming from the batch %d and this experiment started %v ago.\n",
+					localQueue, localBatch, time.Since(launchStarted))
+				notifEnded <- true // Tell the main goroutine this worker is done
 			}()
 		}
 	}
